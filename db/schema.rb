23fa3ac2ae9092d0d1f74445fd9527e97ce3e379
Henrique Rodrigues <henriquecs16a@gmail.com>
2022-01-27 21:13:01 +0000

    Creates Game Mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_210048) do
+ActiveRecord::Schema.define(version: 2022_01_27_210714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "game_modes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.integer "total_points"
+    t.integer "total_locations"
+    t.jsonb "configuration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
